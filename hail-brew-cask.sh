934f6c10ed2cdf4555a256b00362bf9cd36dc673
Kurniawan Haikal <conecones@gmail.com>
2015-06-21 22:02:58 +0700

    Initial commit

---
+++ hail-brew-cask.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Install Cask packages
+brew-cask install \
+    chromium \
+    iterm2 \
+    python \
+    git \
+    vox \
+    vox-preferences-pane \
+    box-sync \
+    todoist \
+    istat-menus \
+    macs-fan-control \
+    atom \
+    macdown \
+    mplayerx \
+    adium \
+    unrarx \
+    spectacle \
+    skype \
+    flashlight \
+    slack \
+    android-file-transfer
+
---
+++ install-oh-my-zsh.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
